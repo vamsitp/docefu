796aa984778860849ccc48a40820eeec854c4dcf
Vamsi TP <vamsitp@microsoft.com>
2018-12-01 12:43:16 +0530

    Accepting VSTS details on the console

--- EpicDoc/Program.cs
+++ EpicDoc/Program.cs
@@ -14,6 +14,8 @@
     using System.Text.RegularExpressions;
     using System.Threading.Tasks;
 
+    using ColoredConsole;
+
     using Newtonsoft.Json;
 
     using Word = Microsoft.Office.Interop.Word;
@@ -24,12 +26,13 @@
         private const string WorkItemsQueryPath = "wit/workitems?ids={0}&api-version=4.1-preview"; //"queries/Shared Queries/EFUs";
         private const string SecurityTokensUrl = "_details/security/tokens";
 
-        private static readonly string Account = ConfigurationManager.AppSettings["Account"];
-        private static readonly string Project = ConfigurationManager.AppSettings["Project"];
-        private static readonly string AreaPath = ConfigurationManager.AppSettings["AreaPath"];
+        private static string Account => ConfigurationManager.AppSettings["Account"];
+        private static string Project => ConfigurationManager.AppSettings["Project"];
+        private static string Token => ConfigurationManager.AppSettings["PersonalAccessToken"];
+
         private static readonly string WorkItemsQuery = ConfigurationManager.AppSettings["WorkItemsQuery"];
         private static readonly string WordTemplate = ConfigurationManager.AppSettings["WordTemplate"];
-        private static readonly string PersonalAccessToken = ConfigurationManager.AppSettings["PersonalAccessToken"];
+
         private static readonly string HeadersColor = ConfigurationManager.AppSettings["HeadersColor"];
         private static readonly string[] ColorReplacements = ConfigurationManager.AppSettings["ColorReplacements"].Split(new[] { ';' }, StringSplitOptions.RemoveEmptyEntries);
         private const string WorkItemsJson = "./WorkItems.json";
@@ -41,12 +44,12 @@
         [STAThread]
         public static void Main(string[] args)
         {
-            MainAsync(args).Wait();
+            Execute(args).Wait();
             GenerateDoc(string.Join(HtmlNewLine, efus.Select(GetContent)));
             Console.ReadLine();
         }
 
-        public static async Task MainAsync(string[] args)
+        public static async Task Execute(string[] args)
         {
             try
             {
@@ -58,12 +61,14 @@
                     }
                     else
                     {
-                        Console.WriteLine($"{WorkItemsJson} not found! Fetching details from VSTS...");
+                        ColorConsole.WriteLine($"{WorkItemsJson} not found! Fetching details from VSTS...".Yellow());
                         await ProcessWorkItems();
                     }
                 }
                 else
                 {
+                    CheckSettings();
+                    ColorConsole.WriteLine($"You can use an existing {WorkItemsJson} file as an input using '-i'".Yellow());
                     await ProcessWorkItems();
                 }
             }
@@ -73,34 +78,44 @@
             }
         }
 
+        private static void CheckSettings()
+        {
+            if (string.IsNullOrWhiteSpace(Account) || string.IsNullOrWhiteSpace(Project) || string.IsNullOrWhiteSpace(Token))
+            {
+                ColorConsole.WriteLine("\n Please provide Azure DevOps details in the format (without braces): <Account> <Project> <PersonalAccessToken>".Black().OnCyan());
+                var details = Console.ReadLine().Split(' ')?.ToList();
+                var config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+                var section = config.Sections.OfType<AppSettingsSection>().FirstOrDefault();
+                var settings = section.Settings;
+
+                for (var i = 0; i < 3; i++) // Only 4 values required
+                {
+                    var key = settings.AllKeys[i];
+                    settings[key].Value = details[i];
+                }
+
+                config.Save(ConfigurationSaveMode.Minimal);
+                ConfigurationManager.RefreshSection(section.SectionInformation.Name);
+            }
+        }
+
         private static async Task ProcessWorkItems(bool local = false)
         {
             if (local)
             {
                 efus = JsonConvert.DeserializeObject<List<EFU>>(File.ReadAllText(WorkItemsJson));
-                Console.WriteLine($"Loaded {efus?.Count} Work-items from {WorkItemsJson}");
+                ColorConsole.WriteLine($"Loaded {efus?.Count} Work-items from {WorkItemsJson}");
                 await GetWorkItems();
             }
             else
             {
-                if (string.IsNullOrWhiteSpace(PersonalAccessToken))
-                {
-                    var tokenUrl = $"{Account}{SecurityTokensUrl}";
-                    Console.WriteLine($"PersonalAccessToken is blank in the config!\nHit ENTER to generate it (select 'All Scopes') at: {tokenUrl}");
-                    Console.ReadLine();
-                    Process.Start(tokenUrl);
-                    Console.WriteLine($"Update 'PersonalAccessToken' value in EpicDoc.exe.config with the generated/copied token and restart the app.");
-                }
-                else
-                {
-                    await GetWorkItems();
-                }
+                await GetWorkItems();
             }
         }
 
         private static void WriteError(string error)
         {
-            Console.WriteLine("Error: " + error);
+            ColorConsole.WriteLine($"Error: {error}".White().OnRed());
         }
 
         private static async Task GetWorkItems()
@@ -119,8 +134,8 @@
             if (efus == null)
             {
                 efus = new List<EFU>();
-                var wis = await GetData<WiqlRelationList>(RelationsQueryPath, Project, "{\"query\": \"" + string.Format(WorkItemsQuery, Project, string.IsNullOrWhiteSpace(AreaPath) ? Project : AreaPath) + "\"}");
-                Console.WriteLine($"Work-item relations fetched: {wis.workItemRelations.Length}");
+                var wis = await GetData<WiqlRelationList>(RelationsQueryPath, Project, "{\"query\": \"" + string.Format(WorkItemsQuery, Project) + "\"}"); // AND [Source].[System.AreaPath] UNDER '{1}'
+                ColorConsole.WriteLine($"Work-item relations fetched: {wis.workItemRelations.Length}");
                 var rootItems = wis.workItemRelations.Where(x => x.source == null).ToList();
                 await IterateWorkItems(rootItems, null, wis);
                 File.WriteAllText(WorkItemsJson, JsonConvert.SerializeObject(efus));
@@ -134,7 +149,7 @@
                 var workitems = await GetWorkItems(relations.ToList());
                 foreach (var wi in workitems)
                 {
-                    Console.WriteLine($" {wi.fields.SystemWorkItemType} ".PadRight(13) + wi.id.ToString().PadLeft(4) + Dot + wi.fields.SystemTitle + $" [{wi.fields.SystemTags}]");
+                    ColorConsole.WriteLine($" {wi.fields.SystemWorkItemType} ".PadRight(13) + wi.id.ToString().PadLeft(4) + Dot + wi.fields.SystemTitle + $" [{wi.fields.SystemTags}]");
                     var efu = new EFU
                     {
                         Id = wi.id,
@@ -158,7 +173,7 @@
             var result = await GetData<WorkItem>(item.url, Project, string.Empty);
             if (result != null)
             {
-                Console.WriteLine(result.id.ToString().PadLeft(4) + $" {result.fields.SystemWorkItemType} - ".PadLeft(14) + result.fields.SystemTitle);
+                ColorConsole.WriteLine(result.id.ToString().PadLeft(4) + $" {result.fields.SystemWorkItemType} - ".PadLeft(14) + result.fields.SystemTitle);
                 return result;
             }
 
@@ -212,18 +227,18 @@
             {
                 workItems = new List<WorkItem>();
                 // var wis = await GetData<WiqlList>(WorkItemsQueryPath, Project, string.Empty);
-                var wis = await GetData<WiqlList>(RelationsQueryPath, Project, "{\"query\": \"" + string.Format(WorkItemsQuery, Project, string.IsNullOrWhiteSpace(AreaPath) ? Project : AreaPath) + "\"}");
+                var wis = await GetData<WiqlList>(RelationsQueryPath, Project, "{\"query\": \"" + string.Format(WorkItemsQuery, Project) + "\"}");
                 foreach (var wi in wis.workItems)
                 {
                     var result = await GetData<WorkItem>(wi.url, Project, string.Empty);
                     if (result != null)
                     {
-                        Console.WriteLine(result.id.ToString().PadLeft(4) + ": " + result.fields.SystemTitle);
+                        ColorConsole.WriteLine(result.id.ToString().PadLeft(4) + ": " + result.fields.SystemTitle);
                         workItems.Add(result);
                     }
                     else
                     {
-                        Console.WriteLine($"Unable to fetch details for: {wi.id}");
+                        ColorConsole.WriteLine($"Unable to fetch details for: {wi.id}".Red());
                     }
                 }
 
@@ -235,7 +250,7 @@
 
         private static void GenerateDoc(string content)
         {
-            Console.WriteLine($"Generating Document from Work-items...");
+            ColorConsole.WriteLine($"Generating Document from Work-items...".Cyan());
             var wordApp = new Word.Application { Visible = false, DisplayAlerts = Word.WdAlertLevel.wdAlertsNone, ScreenUpdating = false };
             object fileName = Path.Combine(Environment.CurrentDirectory, WordTemplate);
             object missing = Type.Missing;
@@ -250,7 +265,7 @@
             var bookmark = wordDoc.Bookmarks.get_Item(1);
             ReplaceBookmark(bookmark.Range, value);
 
-            object saveTo = Path.Combine(Environment.CurrentDirectory, "FuncSpec (UserStories).docx");
+            object saveTo = GetFullPath("FuncSpec (UserStories).docx");
             object format = Word.WdSaveFormat.wdFormatXMLDocument;
 
             object start = wordDoc.Content.Start;
@@ -264,7 +279,9 @@
             NAR(wordDoc);
             wordApp.Quit(ref missing, ref missing, ref missing);
             NAR(wordApp);
-            Console.WriteLine($"Done creating {saveTo}");
+            File.Delete(GetFullPath("temp.html"));
+            ColorConsole.WriteLine($"Done creating {saveTo}");
+            Process.Start("cmd", $"/c \"{saveTo}\"");
         }
 
         private static string GetContent(EFU efu)
@@ -311,9 +328,14 @@
 
             rng.Font.Name = "Segoe UI";
             rng.Font.Size = 11;
-            var file = Path.Combine(Path.GetDirectoryName(Assembly.GetCallingAssembly().CodeBase.Replace("file:///", string.Empty)), "temp.html");
+            var file = GetFullPath("temp.html");
             File.WriteAllText(file, html);
             rng.InsertFile(file);
+        }
+
+        private static string GetFullPath(string file)
+        {
+            return Path.Combine(Path.GetDirectoryName(Assembly.GetCallingAssembly().CodeBase.Replace("file:///", string.Empty)), file);
         }
 
         protected static void NAR(object o)
@@ -336,8 +358,8 @@
             // https://www.visualstudio.com/en-us/docs/integrate/api/wit/samples
             using (var client = new HttpClient())
             {
-                var credentials = Convert.ToBase64String(Encoding.ASCII.GetBytes($":{PersonalAccessToken}"));
-                client.BaseAddress = new Uri(Account);
+                var credentials = Convert.ToBase64String(Encoding.ASCII.GetBytes($":{Token}"));
+                client.BaseAddress = new Uri(Account.IndexOf(".com", StringComparison.OrdinalIgnoreCase) > 0 ? Account : $"https://{Account}.visualstudio.com");
                 client.DefaultRequestHeaders.Accept.Clear();
                 client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
                 client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", credentials);
